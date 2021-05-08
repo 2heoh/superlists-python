d6cb42e81c5aeaf3322b09e80a5824abfd4152fb
2heoh <sergey.lobin@gmail.com>
2021-05-08 20:28:35 +0300

    Moving to unittest framework

--- functional_tests.py
+++ functional_tests.py
@@ -1,11 +1,15 @@
-
-
-
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Chrome('./bin/chromedriver')
-browser.get('http://localhost:8000')
 
-assert 'worked' in browser.title
+class NewVisitorTest(unittest.TestCase):
 
-browser.quit()
+    def setUp(self):
+        self.browser = webdriver.Chrome('./bin/chromedriver')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_return_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
